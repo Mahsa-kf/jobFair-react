8cb56a0adf0f8a2fb0ed98770f9c92f848a08cb7
Mahsa Karim Fard <Mahsa.kf@yahoo.com>
2021-06-11 23:34:50 -0400

    Added new company logoes + updated style

--- src/components/employers/Partners.js
+++ src/components/employers/Partners.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const LogoNames = ['rbcroyalbank.com', 'bell.ca', 'bestlifeonline.com', 'aircanada.com', 'smartcentres.com', 'sloc.co.uk', 'cibc.com', 'friendlyfuture.com','humber.ca','rogers.com'];
+const LogoNames = [
+    'bell.ca', 'rbcroyalbank.com', 'bestlifeonline.com', 'aircanada.com', 'smartcentres.com', 'sloc.co.uk', 'cibc.com', 'friendlyfuture.com','humber.ca',
+    'rogers.com','sunlife.ca','adidas-group.com','manulife.com','airbnb.com', 'adp.com', 'fedex.com', 'thegatewaypundit.com', 'hbo.com', 'ksl.com', 'lowes.com',
+    'pcmag.com', 'puma.com', 
+];
 
 const Partners = () => {
     return(
-        <div>
+        <div className="center_wrapper">
             {LogoNames.map((logoName, index)=> 
-                <img height="100" key={index} src={`//logo.clearbit.com/${logoName}`} alt={logoName}/>
+                <img className="company-logo" key={index} src={`//logo.clearbit.com/${logoName}`} alt={logoName}/>
             )}
         </div>
     )
